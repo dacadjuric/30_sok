8144ac86129919718da3a7daa1e799ca2e477338
Danijela Djuric <danijeladjuric@Danijelas-MacBook-Pro.local>
2024-12-18 19:07:59 +0100

    final changes to comunity_section

--- components/community-section/community-section.js
+++ components/community-section/community-section.js
@@ -7,16 +7,21 @@ import ProductHuntIcon from "../icons/product-hunt";
 
 export default function CommunitySection() {
   return (
-    <div className="relative min-h-[768px] container mx-auto flex flex-col justify-center items-center px-8">
-      <div className="absolute -top-9 rotate-180">
-        <SquiggleDivider width={340} className="animate-shock" />
+    <div className="relative min-h-[100vh] sm:min-h-[768px] container mx-auto flex flex-col justify-center items-center px-8">
+      <div className="absolute -top-12 sm:-top-20 md:-top-16 rotate-180 ">
+        <SquiggleDivider
+          width={340}
+          className="animate-shock w-[200px] sm:w-full"
+        />
       </div>
-      <div className="w-[50%]">
-        <h4 className="text-2xl text-center">Trusted by</h4>
-        <h1 className="text-6xl font-extrabold text-center">25K+ DEVELOPERS</h1>
+      <div className="sm:w-[50%]">
+        <h4 className="text-lg sm:text-2xl text-center">Trusted by</h4>
+        <h1 className="text-5xl sm:text-6xl font-extrabold text-center">
+          25K+ DEVELOPERS
+        </h1>
       </div>
 
-      <div className="gap-12 flex flex-row flex-nowrap justify-start text-center mt-9 mb-11">
+      <div className="gap-6 sm:gap-12 flex flex-row flex-nowrap max-lg:flex-col justify-start text-center mt-9 mb-11">
         <DownloadCard />
 
         <div className="w-80 px-6 py-7 rounded-lg bg-yellow transition transform hover:scale-105">
@@ -58,7 +63,7 @@ export default function CommunitySection() {
           </a>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center max-lg:pb-20">
         <ReviewStars width={320} className="rotate-180" />
       </div>
     </div>
