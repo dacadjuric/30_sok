074f88c18187be08a1274242ae9f1aa2f3673273
Danijela Djuric <danijeladjuric@Danijelas-MacBook-Pro.local>
2024-12-18 19:10:02 +0100

    final changes to snippets page

--- components/snippets/snippets.js
+++ components/snippets/snippets.js
@@ -1,11 +1,18 @@
+"use client";
+
+import { useWindowSize } from "@uidotdev/usehooks";
 import InfiniteGridScroller from "../infinite-grid-scroller/infinite-grid-scroller";
 
 export default function SnippetsPage() {
+  const size = useWindowSize();
+  const rows = size.width >= 640 ? 5 : 3;
   return (
-    <div className="relative min-h-[768px] container mx-auto flex flex-row justify-center items-center px-8">
-      <div className="w-[50%]">
-        <h1 className="text-6xl font-extrabold">1500+ SNIPPETS</h1>
-        <h3>
+    <div className="relative min-h-[100vh] sm:min-h-[768px] container mx-auto flex flex-col sm:flex-row justify-center items-center px-8">
+      <div className="sm:w-[50%] mb-3">
+        <h2 className="text-5xl font-bold text-center sm:text-6xl sm:font-extrabold sm:text-left">
+          1500+ SNIPPETS
+        </h2>
+        <div className="text-sm text-center sm:text-base sm:text-left">
           In <span className="font-bold text-yellow">14 Snippet Libraries</span>{" "}
           consisted of{" "}
           <span className="font-bold text-yellow">Programming Languages</span>,
@@ -14,13 +21,13 @@ export default function SnippetsPage() {
           <span className="font-bold text-yellow"> Interview Questions</span>,
           <br />
           with more to come soon!
-        </h3>
+        </div>
         <br />
-        <p className="italic text-xs">
+        <p className="italic text-xs text-center sm:text-left">
           (Pro tip: CSS Snippets support Live Preview)
         </p>
       </div>
-      <InfiniteGridScroller />
+      <InfiniteGridScroller rows={rows} />
     </div>
   );
 }
