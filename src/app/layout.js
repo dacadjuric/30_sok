410723ca42e502999f213707fc47f935e16d1c8d
Danijela Djuric <danijeladjuric@Danijelas-MacBook-Pro.local>
2024-12-18 19:04:15 +0100

    final changes to json files and layout and css in app

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,44 @@ const roboto = Roboto_Flex({ subsets: ["latin"], variable: "--font-roboto" });
 const teko = Teko({ subsets: ["latin"], variable: "--font-teko" });
 
 export const metadata = {
-  title: "30 Seconds Of Knowledge",
+  title: {
+    template: "%s | 30 Seconds Of Knowledge",
+    default: "30 Seconds Of Knowledge",
+  },
   description: "Landing page for 30 seconds of knowledge",
+  keywords: [
+    "developer",
+    "knowledge",
+    "30 seconds",
+    "react",
+    "JS",
+    "Ruby",
+    "JavaScript",
+  ],
+  authors: [
+    { name: "Danijela Djuric Petrovic", url: "https://github.com/dacadjuric" },
+  ],
+  siteName: "30 Seconds Of Knowledge",
+  locale: "en_US",
+  type: "website",
+  icons: {
+    icon: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
+    shortcut: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
+    apple: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
+  },
+  openGraph: {
+    title: "30 Seconds Of Knowledge",
+    description:
+      "With 30 Seconds Of Knowledge web extension become a better developer one new tab at a time",
+    url: "https://www.30secondsofknowledge.com/",
+    images: {
+      url: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      alt: "30 Seconds Of Knowledge logo photo",
+      width: 1280,
+      height: 800,
+      type: "image/png",
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
