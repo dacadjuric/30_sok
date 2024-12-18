b04431bad18521086b070f38301c3075eccc3576
Danijela Djuric <danijeladjuric@Danijelas-MacBook-Pro.local>
2024-12-18 19:06:27 +0100

    final changes to download btn

--- components/download-button/download-button.js
+++ components/download-button/download-button.js
@@ -13,19 +13,33 @@ function DownloadButton(props) {
 
   return (
     <a
-      className="mx-24 bg-textLight rounded-lg border-textLight pt-3 pr-5 pb-3 pl-4 flex place-items-center gap-x-2 text-background uppercase"
+      className="mx-16 sm:mx-24 sm:max-w-[320px] bg-textLight rounded-lg border-textLight pt-3 pr-5 pb-3 pl-4 flex place-items-center sm:gap-x-2 text-background uppercase"
       target="_blank"
       href={downloadUrl}
       {...props}
     >
-      <span className="flex items-center">
-        <Image src={firefoxIcon} alt="Firefox icon" className="-mr-6 size-12" />
-        <Image src={edgeIcon} alt="Edge icon" className="-mr-6 size-12" />
-        <Image src={chromeIcon} alt="Chrome icon" className=" size-12" />
+      <span className="hidden sm:visible sm:flex items-center">
+        <Image
+          src={firefoxIcon}
+          alt="Firefox icon"
+          className="size-8 -mr-3 sm:-mr-6 sm:size-12"
+        />
+        <Image
+          src={edgeIcon}
+          alt="Edge icon"
+          className="size-8 -mr-3 sm:-mr-6  sm:size-12"
+        />
+        <Image
+          src={chromeIcon}
+          alt="Chrome icon"
+          className="size-8 sm:size-12"
+        />
       </span>
-      <div className="flex flex-col items-start">
-        <span className="text-lg leading-6 font-bold">start learning </span>
-        <span className="text-sm leading-[18px] font-medium text-ctaLightColor">
+      <div className="flex flex-col">
+        <span className="text-base leading-4 sm:text-lg sm:leading-6 font-bold">
+          start learning{" "}
+        </span>
+        <span className="text-sm leading-4 font-medium text-ctaLightColor">
           it's free
         </span>
       </div>
