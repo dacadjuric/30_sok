a5ea474dafb107b06862c0c5d41a01fa5b23ddd3
Danijela Djuric <danijeladjuric00@gmail.com>
2025-03-05 17:58:27 +0100

    added meta images and tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,43 +10,80 @@ const roboto = Roboto_Flex({ subsets: ["latin"], variable: "--font-roboto" });
 const teko = Teko({ subsets: ["latin"], variable: "--font-teko" });
 
 export const metadata = {
+  metadataBase: new URL("https://www.30secondsofknowledge.com"),
   title: {
     template: "%s | 30 Seconds Of Knowledge",
     default: "30 Seconds Of Knowledge",
   },
-  description: "Landing page for 30 seconds of knowledge",
+
+  description:
+    "Boost your coding skills in just 30 seconds! 30 Seconds of Knowledge delivers quick, high-quality code snippets and interview questions on every new tab.",
+
   keywords: [
     "developer",
-    "knowledge",
+    "learning",
+    "nextjs",
     "30 seconds",
     "react",
     "JS",
     "Ruby",
     "JavaScript",
   ],
-  authors: [
-    { name: "Danijela Djuric Petrovic", url: "https://github.com/dacadjuric" },
-  ],
+  referrer: "strict-origin-when-cross-origin",
   siteName: "30 Seconds Of Knowledge",
   locale: "en_US",
   type: "website",
   icons: {
-    icon: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
-    shortcut: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
-    apple: "https://cdn-icons-png.flaticon.com/512/2815/2815428.png",
+    icon: "../../assets/metaImages/icons/favicon.ico",
+    shortcut: "../../assets/metaImages/icon.png",
+    apple: "../../assets/metaImages/icons/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
   openGraph: {
     title: "30 Seconds Of Knowledge",
     description:
-      "With 30 Seconds Of Knowledge web extension become a better developer one new tab at a time",
+      "Boost your coding skills in just 30 seconds! 30 Seconds of Knowledge delivers quick, high-quality code snippets and interview questions on every new tab.",
     url: "https://www.30secondsofknowledge.com/",
-    images: {
-      url: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-      alt: "30 Seconds Of Knowledge logo photo",
-      width: 1280,
-      height: 800,
-      type: "image/png",
-    },
+    siteName: "30 Seconds Of Knowledge",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "../../assets/open_graph.jpg",
+        alt: "30 Seconds Of Knowledge photo",
+        width: 800,
+        height: 600,
+        type: "image/jpg",
+      },
+      {
+        url: "../../assets/schema.jpg",
+        alt: "30 Seconds Of Knowledge photo",
+        width: 1800,
+        height: 1600,
+        type: "image/jpg",
+      },
+    ],
+    updatedTime: "2025-02-04T12:00:00Z",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "30 Seconds of Knowledge",
+    description:
+      "Learn to code faster! Get bite-sized coding knowledge with high-quality snippets on every new tab. Master JavaScript, Python, React, and more.",
+    image: "../../assets/twitter.jpg",
+    imageAlt: "Banner image for the website 30 Seconds Of Knowledge with logo",
   },
 };
 
